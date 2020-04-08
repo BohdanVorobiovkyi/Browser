ea87e6b29206cd4d6f6c7a424e17500cc7b1d422
Bohdan <Vorobiovkyi>
2020-04-08 15:11:54 +0300

    Initial Commit

---
+++ Browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Browser
+//
+//  Created by usr01 on 08.04.2020.
+//  Copyright © 2020 bhdn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
